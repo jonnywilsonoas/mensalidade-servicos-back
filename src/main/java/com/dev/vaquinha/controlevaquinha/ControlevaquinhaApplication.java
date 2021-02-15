e2a44b87515a70a10a4f1412781f7ffa9345bcab
jonnywoas <jonny.jw1@gmail.com>
2021-02-14 21:09:34 -0300

    Initial commit

---
+++ src/main/java/com/dev/vaquinha/controlevaquinha/ControlevaquinhaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.vaquinha.controlevaquinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlevaquinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlevaquinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/vaquinha/controlevaquinha/ControlevaquinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.vaquinha.controlevaquinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlevaquinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
